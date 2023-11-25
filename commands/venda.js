39e0341bc7e67623653f90bae2e9f79c10cfef5b
Isabella <isa021andrade@gmail.com>
2023-11-25 10:49:59 -0300

    Refactored 'venda' command to use database for inventory tracking

--- commands/venda.js
+++ commands/venda.js
@@ -1,6 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, Events, ModalBuilder, TextInputBuilder, TextInputStyle } = require("discord.js")
-
-let kitBau = 400
+const db = require('../database/inventory.js')
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -8,56 +7,59 @@ module.exports = {
         .setDescription("Gera um formulário para registro de vendas."),
 
     async execute(interaction){
+        
+        const inv = {
+            id: 'dx6dGwYfYacRso94HkY9'
+        }
+        
         try{
 
-        const modal = new ModalBuilder()
-        .setCustomId('ModalVendas')
-        .setTitle('Registro de Venda');
-    
-        const idInput = new TextInputBuilder()
-            .setCustomId('idInput')
-            .setLabel("Digite o passaporte do cliente")
-            .setStyle(TextInputStyle.Short);
-        const qntdInput = new TextInputBuilder()
-            .setCustomId('qntdInput')
-            .setLabel("Digite a quantidade vendida (APENAS NÚMEROS)")
-            .setStyle(TextInputStyle.Short);
-
-        const firstActionRow = new ActionRowBuilder().addComponents(idInput);
-        const secondActionRow = new ActionRowBuilder().addComponents(qntdInput);
-
-        modal.addComponents(firstActionRow, secondActionRow);
-        await interaction.showModal(modal)
+            const modal = new ModalBuilder()
+            .setCustomId('ModalVendas')
+            .setTitle('Registro de Venda');
         
-        const filter = (interaction) => interaction.customId === 'ModalVendas'
-        interaction
-        .awaitModalSubmit({filter, time: 280_000})
-        .then((modalInteraction) => {
-            const idValue = modalInteraction.fields.getTextInputValue('idInput');
-            const qntdValue = modalInteraction.fields.getTextInputValue('qntdInput');
+            const idInput = new TextInputBuilder()
+                .setCustomId('idInput')
+                .setLabel("Digite o ID do cliente")
+                .setStyle(TextInputStyle.Short);
+            const qntdInput = new TextInputBuilder()
+                .setCustomId('qntdInput')
+                .setLabel("Digite a quantidade vendida (APENAS NÚMEROS)")
+                .setStyle(TextInputStyle.Short);
 
-            const exampleEmbed = new EmbedBuilder()
-                .setColor(0x960202)
-                .setTitle("Novo formulário registrado!")
-                .setDescription(`✅ <@${interaction.user.id}> acabou de se registrar!`)
-                .setThumbnail(interaction.user.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
-                .addFields(
-                    { name: '\u200B', value: '\u200B' },
-                    { name: 'ID:', value: `${idValue}`},
-                    { name: 'Tinha: ', value: `${kitBau}`, inline: true},
-                    { name: 'Tem:', value: `${kitBau-qntdValue}`, inline: true},
-                    { name: 'Quantidade vendida: ', value: `${qntdValue}`, inline: true},
-                    { name: 'Valor da venda: ', value: `${qntdValue*6}k`,inline: true},
-                    { name: '\u200B', value: '\u200B' },
-                    { name: 'Quem vendeu:', value: `<@${interaction.user.id}>`}
-                )
-                .setTimestamp()
-            modalInteraction.reply({embeds: [exampleEmbed]})
-            //estou tentando descobrir como manter a quantidade kit bau girando
-        })
-        .catch((err) => {
-            console.log('Erro');
-        });
+            const firstActionRow = new ActionRowBuilder().addComponents(idInput);
+            const secondActionRow = new ActionRowBuilder().addComponents(qntdInput);
+            
+            modal.addComponents(firstActionRow, secondActionRow);
+            await interaction.showModal(modal)
+
+            const filter = (interaction) => interaction.customId === 'ModalVendas'
+            interaction
+            .awaitModalSubmit({filter, time: 280_000})
+            .then(async (modalInteraction) => {
+                const idValue = modalInteraction.fields.getTextInputValue('idInput');
+                const qntdValue = modalInteraction.fields.getTextInputValue('qntdInput');
+                const estoqueAntigo = await db.findInventory(inv)
+                const estoqueNovo = estoqueAntigo - qntdValue
+                
+                db.updateInventory(inv, estoqueNovo)
+                const exampleEmbed = new EmbedBuilder()
+                    .setColor(0x960202)
+                    .setTitle("Nova venda realizada!")
+                    .setThumbnail(interaction.user.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
+                    .addFields(
+                        { name: 'ID do comprador:', value: `${idValue}`},
+                        { name: 'Tinha: ', value: `${estoqueAntigo}`, inline: true},
+                        { name: 'Tem:', value: `${estoqueNovo}`, inline: true},
+                        { name: 'Quantidade vendida: ', value: `${qntdValue}`, inline: true},
+                        { name: 'Valor da venda: ', value: `${qntdValue*6}k`,inline: true},
+                    )
+                    .setTimestamp()
+                modalInteraction.reply({content: `<@${interaction.user.id}> acabou de fazer uma venda!`,embeds: [exampleEmbed]})
+            })
+            .catch((err) => {
+                console.log('Erro');
+            });
         
         } catch (error) {
             console.error('Error displaying modal:', error);
